6c7e29520a03715983f656848635a9930a3bf952
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-04-27 14:06:14 -0700

    Initial Commit

---
+++ PaperNote/PaperNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperNoteApp.swift
+//  PaperNote
+//
+//  Created by Morris Richman on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct PaperNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
